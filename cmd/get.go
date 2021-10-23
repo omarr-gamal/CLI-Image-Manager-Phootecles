0ec015b77a305af73722785bcf674281268b30f8
Omar Gamal AbdelMageed <56756748+fiend361@users.noreply.github.com>
2021-10-23 18:30:29 +0200

    finished getting images functionality

--- cmd/get.go
+++ cmd/get.go
@@ -18,10 +18,8 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"net/http"
-	"os"
+	"unicode"
 
 	"github.com/spf13/cobra"
 )
@@ -62,55 +60,37 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		var gopherName = "dr-who.png"
-
 		if len(args) != 1 {
 			fmt.Println("Error:Invalid number of parameters. Try running get -h for help.")
 			return
 		}
 
-		URL := "https://github.com/scraly/gophers/raw/main/" + gopherName + ".png"
-
-		fmt.Println("Try to get '" + gopherName + "' Gopher...")
-
-		// Get the data
-		response, err := http.Get(URL)
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer response.Body.Close()
-
-		if response.StatusCode == 200 {
-			// Create the file
-			out, err := os.Create(gopherName + ".png")
-			if err != nil {
-				fmt.Println(err)
+		if args[0] == "all" {
+			images := getImages()
+			for _, img := range images {
+				formatImage(img)
 			}
-			defer out.Close()
-
-			// Writer the body to file
-			_, err = io.Copy(out, response.Body)
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			fmt.Println("Perfect! Just saved in " + out.Name() + "!")
+			fmt.Printf("Number of images is %v", len(images))
 		} else {
-			fmt.Println("Error: " + gopherName + " not exists! :-(")
+			if !isNumber(args[0]) {
+				fmt.Println("Error:Invalid image id.")
+				return
+			}
+			images := getImages()
+			for _, img := range images {
+				if img.Id == args[0] {
+					formatImage(img)
+					return
+				}
+			}
+			fmt.Printf("Error:Couldn't find image with id \"%v\"", args[0])
 		}
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(getCmd)
-	// i := Image{
-	// 	Id:             "ded",
-	// 	Title:          "ded",
-	// 	Description:    "ded",
-	// 	ImageOnlineUrl: "ded",
-	// }
-	// print(i)
-
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
@@ -120,4 +100,21 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func formatImage(img Image) {
+	fmt.Printf("id:%v\n", img.Id)
+	fmt.Printf("title:%v\n", img.Title)
+	fmt.Printf("description:%v\n", img.Description)
+	fmt.Printf("url:%v\n", img.ImageOnlineUrl)
+	println("---------------")
+}
+
+func isNumber(s string) bool {
+	for _, c := range s {
+		if !unicode.IsDigit(c) {
+			return false
+		}
+	}
+	return true
 }
